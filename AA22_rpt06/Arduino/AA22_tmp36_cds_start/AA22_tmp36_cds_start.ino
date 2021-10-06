5ab86640a487acf619efbaeb9743ccfc3737f930
kadilan <yes235817@gmail.com>
2021-10-06 16:34:42 +0900

    Add files via upload

--- AA22_rpt06/Arduino/AA22_tmp36_cds_start/AA22_tmp36_cds_start.ino
+++ AA22_rpt06/Arduino/AA22_tmp36_cds_start/AA22_tmp36_cds_start.ino
@@ -13,22 +13,29 @@ void setup() {
 void loop() {
    //getting the voltage reading from the temperature sensor
   int value = analogRead(TMP36_INPUT);
-  Serial.print("value = ");
-  Serial.println(value);
+//  Serial.print("value = ");
+//  Serial.println(value);
 //  Serial.print(" : ");
  
    // converting that reading to voltage
-//   float voltage = value * 5.0 * 1000;  // in mV
-//   voltage /= 1023.0; 
+   float voltage = value * 5.0 * 1000;  // in mV
+   voltage /= 1023.0; 
    
    // print out the voltage
 //   Serial.print(voltage); 
 //   Serial.print(" mV, ");
    
    // now print out the temperature
-//   float temperatureC = (voltage - 500) / 10 ;  
-//   Serial.print(temperatureC); 
+   float temperatureC = (voltage - 500) / 10 ; 
+   Serial.print(temperatureC); 
 //   Serial.println(" degrees C");
+
+  Serial.print(", ");
+
+  int cds_value = analogRead(CDS_INPUT);
+  int lux = int(luminosity(cds_value));
+
+  Serial.println(lux);
 
   delay(1000);
 }
